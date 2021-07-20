03f96a92175c198cd464aea9da57000fe5ae7d64
James Grande <james.grande6@gmail.com>
2021-07-19 23:00:28 -1000

    Add status input and buttons, and refactor code

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.status = status;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
-
-addInitialBooks();
-displayBooksInLibrary();
 
 function displayBooksInLibrary() {
     myLibrary.forEach(book => displayNewBook(book));
@@ -25,36 +23,44 @@ function displayNewBook(book) {
     const tableData1 = document.createElement("td");
     const tableData2 = document.createElement("td");
     const tableData3 = document.createElement("td");
+    const tableData4 = document.createElement("td");
+    const statusButton = document.createElement("button");
 
     tableData1.textContent = book.title;
     tableData2.textContent = book.author;
     tableData3.textContent = book.pages;
+
+    statusButton.type = "button";
+    statusButton.className = "status-buttons"
+    statusButton.id = book.status ? "read" : "not-read";
+    statusButton.textContent = book.status ? "Read" : "Not Read";
+
+    tableData4.appendChild(statusButton);
     
     tableRow.appendChild(tableData1);
     tableRow.appendChild(tableData2);
     tableRow.appendChild(tableData3);
+    tableRow.appendChild(tableData4);
 
     table.appendChild(tableRow);
+
+    addStatusButtonListener(statusButton);
 }
 
-function addInitialBooks() {
-    const book1 = new Book("Harry Potter", "J. K. Rowling", 336);
-    const book2 = new Book("The Hobbit", "J. R. R. Tolkien", 304);
-
-    addBookToLibrary(book1);
-    addBookToLibrary(book2);
-}
+addSubmitButtonListener();
 
 function addSubmitButtonListener() {
     const submitButton = document.querySelector(".submit-button");
     const title = document.querySelector("#title");
     const author = document.querySelector("#author");
     const pages = document.querySelector("#pages");
+    const status = document.querySelector("#status");
 
     submitButton.addEventListener("click", () => {
         
-        if (title.value.length && author.value.length && pages.value.length) {
-            const book = new Book(title.value, author.value, pages.value);
+        if (title.value.length) {
+            const book = new Book(title.value, author.value, pages.value,
+                    (status.value === "read"));
 
             addBookToLibrary(book);
             displayNewBook(book);
@@ -62,5 +68,33 @@ function addSubmitButtonListener() {
     });
 }
 
-addSubmitButtonListener();
+addStatusButtonsListener();
 
+function addStatusButtonsListener() {
+    const statusButtons = document.querySelectorAll(".status-buttons");
+
+    statusButtons.forEach(statusButton => addStatusButtonListener(statusButton));
+}
+
+function addStatusButtonListener(statusButton) {
+    statusButton.addEventListener("click", () => {
+        if (statusButton.id === "read") {
+            statusButton.textContent = "Not Read";
+            statusButton.id = "not-read";
+        } else if (statusButton.id === "not-read") {
+            statusButton.textContent = "Read";
+            statusButton.id = "read";
+        }
+    });
+}
+
+function addInitialBooks() {
+    const book1 = new Book("Harry Potter", "J. K. Rowling", 336, false);
+    const book2 = new Book("The Hobbit", "J. R. R. Tolkien", 304, true);
+
+    addBookToLibrary(book1);
+    addBookToLibrary(book2);
+}
+
+addInitialBooks();
+displayBooksInLibrary();
